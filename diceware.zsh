786ea82b9ffc16123529ea46a2275ddb9bc39de4
Jonathan Waldrep <spartan074@gmail.com>
2016-02-01 15:55:46 -0500

    move diceware from a script to a function

---
+++ diceware.zsh
@@ -0,0 +1,16 @@
+#!/usr/bin/zsh
+
+function diceware() {
+  typeset -r WORDFILE="${HOME}/lib/diceware.wordlist.asc"
+
+  if ! gpg --verify "${WORDFILE}" 2>/dev/null; then
+    echo "${WORDFILE} did not pass the integrity check!"
+    return 1
+  fi
+
+  while [[ -n $1 ]]; do
+    echo -n "$(grep "^$1	" "${WORDFILE}" | cut -f 2)"
+    [[ -n $2 ]] && echo -n ' ' || echo ''
+    shift
+  done
+}
