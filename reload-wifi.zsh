6c7b8f46c9fafa395fe4388fcaf3833db4f102c3
Jonathan Waldrep <spartan074@gmail.com>
2016-02-01 17:08:39 -0500

    moved simple scripts to functions

---
+++ reload-wifi.zsh
@@ -0,0 +1,15 @@
+#!/usr/bin/zsh
+
+function reload-wifi() {
+  # unload
+  sudo systemctl stop connman.service
+  sudo systemctl stop wpa_supplicant.service
+  sudo modprobe -r wl
+  
+  # wait for good measure
+  sleep 1
+
+  # load
+  sudo modprobe wl
+  sudo systemctl start connman.service
+}
---
+++ sprng.zsh
@@ -0,0 +1,11 @@
+#!/usr/bin/zsh
+
+function sprng() {
+  typeset -r URL='http://sprunge.us'
+
+  if [ -z $1 ] ; then
+          curl -F 'sprunge=<-' $URL 2&>/dev/null | xclip
+  else
+          curl $URL/$1 2&>/dev/null
+  fi
+}
