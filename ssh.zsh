9b866078b38eba0b4d76c351b14ab7c712e03e12
Jonathan Waldrep <spartan074@gmail.com>
2016-02-01 15:54:58 -0500

    move zsh from .zshrc to a seperate file

---
+++ ssh.zsh
@@ -0,0 +1,36 @@
+#!/usr/bin/zsh
+
+function ssh () {
+  typeset arg=1 
+  typeset logtmp
+  typeset logfile
+  typeset -r logdir="${HOME}/.ssh/logs" 
+  typeset -a args
+  args=("$@") 
+  readonly -a args
+
+  while [[ -z "${logfile}" ]] && [[ "${arg}" -le "${#args}" ]]; do
+    case ${args[arg]} in
+      (-[1246AaCfGgKkMNnqsTtVvXxYy]*)
+        (( arg++ ))
+        ;;
+      (-*)
+        (( arg+=2 ))
+        ;;
+      (*)
+        typeset -r logfile="${logdir}/$(date +%Y%b%d-%T) ${args[arg]}.log.asc"
+        ;;
+    esac
+  done
+
+  if [[ -n "${logfile}" ]]; then
+    typeset -r logtmp="/tmp/${logfile:t:r}" 
+
+    =ssh ${args} | tee >(col -b > "${logtmp}")
+
+    [[ -s "${logtmp}" ]] && gpg --output "${logfile}" -esa "${logtmp}"
+    rm "${logtmp}"
+  else
+    =ssh ${args}
+  fi
+}
