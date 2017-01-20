77c86f174f6b541d939237aef9ac2cac4367f011
Jonathan Waldrep <waldrep@vt.edu>
2017-01-20 08:53:28 -0500

    appropriate exiting method for functions

--- new-script.zsh
+++ new-script.zsh
@@ -1,10 +1,10 @@
 function new-script() {
   if [[ -z $1 ]]; then
     echo 'Enter a file name.' >&2
-    exit 1
+    return 1
   elif [[ -a $1 ]]; then
     echo 'File exists.' >&2
-    exit 1
+    return 1
   else
     echo '#!/usr/bin/zsh'$'\n' > $1
     chmod +x $1
