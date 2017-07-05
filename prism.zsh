59b855119b7bbb53fd5ae0b8fd307c15f57b299a
Jonathan Waldrep <waldrep@vt.edu>
2017-07-05 13:49:51 -0400

    remove alias for broadcom drivers

--- prism.zsh
+++
@@ -1,3 +0,0 @@
-# aliases for putting a broadcom card in/out of monitor mode
-alias wl-mon='echo 1 | sudo tee /proc/brcm_monitor0 > /dev/null'
-alias wl-normal='echo 0 | sudo tee /proc/brcm_monitor0 > /dev/null'
