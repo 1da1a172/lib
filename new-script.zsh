1c82b53dc1c55286bc440dfa3228368c90412a13
Jonathan Waldrep <waldrep@vt.edu>
2020-02-06 13:48:38 -0500

    function for new python scripts

--- new-script.zsh
+++ new-script.zsh
@@ -1,4 +1,4 @@
-function new-script() {
+function nz() {
   if [[ -z $1 ]]; then
     echo 'Enter a file name.' >&2
     return 1
@@ -7,6 +7,32 @@ function new-script() {
     return 1
   else
     echo '#!/usr/bin/zsh'$'\n' > $1
+    chmod +x $1
+    vim $1
+  fi
+}
+
+function np() {
+  if [[ -z $1 ]]; then
+    echo 'Enter a file name.' >&2
+    return 1
+  elif [[ -a $1 ]]; then
+    echo 'File exists.' >&2
+    return 1
+  else
+    typeset contents=''
+    contents+=$'#!/usr/bin/env python3\n'
+    contents+=$'\n'
+    contents+=$'import sys\n'
+    contents+=$'from typing import List\n'
+    contents+=$'\n'
+    contents+=$'def main(argv: List[str]) -> int:\n'
+    contents+=$'    return 0\n'
+    contents+=$'\n\n'
+    contents+=$'if __name__ == \'__main__\':\n'
+    contents+=$'    sys.exit(main(sys.argv))'
+
+    echo $contents > $1
     chmod +x $1
     vim $1
   fi
