b61b87aec30ab880fbfe9b0e0f9863f76fdbe5f4
Jonathan Waldrep <waldrep@vt.edu>
2024-09-07 23:10:04 -0400

    Use return early structure

--- new-script.zsh
+++ new-script.zsh
@@ -2,41 +2,41 @@ function nz() {
   if [[ -z $1 ]]; then
     echo 'Enter a file name.' >&2
     return 1
-  elif [[ -a $1 ]]; then
+  fi
+  if [[ -a $1 ]]; then
     echo 'File exists.' >&2
     return 1
-  else
-    echo $'#!/usr/bin/zsh\n' > $1
-    chmod +x $1
-    vim $1
   fi
+  echo $'#!/usr/bin/zsh\n' > $1
+  chmod +x $1
+  vim $1
 }
 
 function np() {
   if [[ -z $1 ]]; then
     echo 'Enter a file name.' >&2
     return 1
-  elif [[ -a $1 ]]; then
+  fi
+  if [[ -a $1 ]]; then
     echo 'File exists.' >&2
     return 1
-  else
-    typeset contents=''
-    contents+=$'#!/usr/bin/env python3\n'
-    contents+=$'""" !!! SCRAP !!! """\n'
-    contents+=$'\n'
-    contents+=$'import sys\n'
-    contents+=$'from typing import List\n'
-    contents+=$'\n'
-    contents+=$'\n'
-    contents+=$'def _main(argv: List[str]) -> int:\n'
-    contents+=$'    del argv\n'
-    contents+=$'    return 0\n'
-    contents+=$'\n\n'
-    contents+=$'if __name__ == "__main__":\n'
-    contents+=$'    sys.exit(_main(sys.argv))'
-
-    echo $contents > $1
-    chmod +x $1
-    vim $1
   fi
+  typeset contents=''
+  contents+=$'#!/usr/bin/env python3\n'
+  contents+=$'""" !!! SCRAP !!! """\n'
+  contents+=$'\n'
+  contents+=$'import sys\n'
+  contents+=$'from typing import List\n'
+  contents+=$'\n'
+  contents+=$'\n'
+  contents+=$'def _main(argv: List[str]) -> int:\n'
+  contents+=$'    del argv\n'
+  contents+=$'    return 0\n'
+  contents+=$'\n\n'
+  contents+=$'if __name__ == "__main__":\n'
+  contents+=$'    sys.exit(_main(sys.argv))'
+
+  echo $contents > $1
+  chmod +x $1
+  vim $1
 }
