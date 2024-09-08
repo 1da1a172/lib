fdf47c2b00b3a0ba41861ada6108c2256bb2b19a
Jonathan Waldrep <waldrep@vt.edu>
2024-09-07 23:12:11 -0400

    Improve source readability

--- ntlm_hash.zsh
+++ ntlm_hash.zsh
@@ -1,3 +1,6 @@
 ntlm_hash () {
-  echo -n "$1" | iconv -f ASCII -t UTF-16LE | openssl md4 | cut -d ' ' -f 2
+  echo -n "$1" \
+    | iconv -f ASCII -t UTF-16LE \
+    | openssl dgst -provider legacy -md4 \
+    | cut -d ' ' -f 2
 }
